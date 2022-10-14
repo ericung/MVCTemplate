d8df4cf86fb1e0791bd2735fb2f502c613a80230
Eric Ung <ungericw@tutanota.com>
2022-10-14 18:11:57 -0500

    Add project files.

---
+++ MVCTemplate.Web/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTemplate.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
